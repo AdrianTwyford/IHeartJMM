3059fd518da8cef00f8a5fcff660967780f3f543
hamishdickson <hamish.dickson@gmail.com>
2015-04-12 19:50:10 +0100

    refactor out classes controller

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -4,7 +4,38 @@
 (function () {
     var app = angular.module('main-controller', []);
 
-    app.controller('MainController', ['$scope', function ($scope) {
+    app.controller('MainController', ['$http', '$rootScope', '$alert', function ($http, $rootScope, $alert) {
 
+        var classes = this;
+
+        classes.classesData = [];
+
+        var url = '/v1/classes.json';
+
+        if ($rootScope.currentUser) {
+            $http.get(url)
+                .success(function (data) {
+                    classes.classesData = data.data.classes;
+                })
+                .error(function () {
+                    $alert({
+                        title: 'Error!',
+                        content: 'Oh dear - there was some kind of server error - please let us know!',
+                        placement: 'top-right',
+                        type: 'danger',
+                        duration: 5
+                    });
+                });
+        }
+
+        this.set = "Dummy Class 1";
+
+        this.isSet = function(name) {
+            return name == this.set;
+        };
+
+        this.setClass = function(name) {
+            this.set = name;
+        };
     }]);
 })();
