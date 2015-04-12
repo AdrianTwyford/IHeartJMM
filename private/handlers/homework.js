b89c36e72f16e59dbde3edf97845fd2d470bbe7a
hamishdickson <hamish.dickson@gmail.com>
2015-04-12 10:28:11 +0100

    move homework stuff about, create teacher resource

---
+++ private/handlers/homework.js
@@ -0,0 +1,98 @@
+/**
+ * Created by hamishdickson on 12/04/15.
+ */
+var helpers = require('./helpers.js');
+
+exports.homework_by_student = function(req, res) {
+    var student = req.params.student;
+
+    helpers.send_success(res,
+        {
+            "student": student,
+            "homeworks" : [
+                {
+                    "subject": "Maths",
+                    "homework": [
+                        {
+                            "homeworkName": "Calculus",
+                            "grade": "95%"
+                        },
+                        {
+                            "homeworkName": "Mechanics",
+                            "grade": "95%"
+                        },
+                        {
+                            "homeworkName": "Statistics",
+                            "grade": "15%"
+                        }
+                    ]
+                },
+                {
+                    "subject": "Biology",
+                    "homework": [
+                        {
+                            "homeworkName": "Whatever biology people do",
+                            "grade": "95%"
+                        }
+                    ]
+                }
+            ]
+        });
+};
+
+exports.homework_by_teacher = function(req, res) {
+    var teacher = req.params.teacher;
+
+    helpers.send_success(res,
+        {
+            "teacher": teacher,
+            "homeworks" : [
+                {
+                    "subject": "Maths",
+                    "homework": [
+                        {
+                            "homeworkName": "Calculus",
+                            "students": [
+                                {
+                                    "name": "bobby",
+                                    "grade": "95%"
+                                }
+                            ]
+                        },
+                        {
+                            "homeworkName": "Mechanics",
+                            "students": [
+                                {
+                                    "name": "bobby",
+                                    "grade": "95%"
+                                }
+                            ]
+                        },
+                        {
+                            "homeworkName": "Statistics",
+                            "students": [
+                                {
+                                    "name": "bobby",
+                                    "grade": "25%"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "subject": "Biology",
+                    "homework": [
+                        {
+                            "homeworkName": "Whatever biology people do",
+                            "students": [
+                                {
+                                    "name": "bobby",
+                                    "grade": "55%"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+};
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var compress = require('compression');
 
 var classes_hdlr = require('./private/handlers/classes.js');
 var students_hdlr = require('./private/handlers/students.js');
+var homework_hdlr = require('./private/handlers/homework.js');
 var login_hdlr = require('./private/handlers/login.js');
 
 // note for AT: this opens up a rest api - if you type in https://mysterious-river-9125.herokuapp.com/v1/classes.json
@@ -19,7 +20,11 @@ app.get('/v1/classes.json', classes_hdlr.list_all);
 /* get the teacher's students */
 app.get('/v1/students/:teacher.json', students_hdlr.students_by_teacher);
 
-app.get('/v1/homework/:student.json', students_hdlr.homework_by_student);
+/* get the homework for a student */
+app.get('/v1/student/homework/:student.json', homework_hdlr.homework_by_student);
+
+/* get the homeworks under a teacher */
+app.get('/v1/teacher/homework/:teacher.json', homework_hdlr.homework_by_teacher);
 
 app.use(compress());
 app.use(bodyParser.json());
